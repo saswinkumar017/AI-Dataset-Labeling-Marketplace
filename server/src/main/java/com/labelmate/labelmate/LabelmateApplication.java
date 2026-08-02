a8b830176635c437df55511e7bc09b1250abe40e
Aswin Kumar S <saswinkumar2007@gmail.com>
2026-08-02 23:11:01 +0530

    chore(backend): initialize spring boot backend

---
+++ server/src/main/java/com/labelmate/labelmate/LabelmateApplication.java
@@ -0,0 +1,13 @@
+package com.labelmate.labelmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelmateApplication.class, args);
+    }
+
+}
